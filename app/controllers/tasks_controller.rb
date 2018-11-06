238798f5e38d78b6c884dc34c049f31dd82523f7
Yuta Fujii <y.f.mar2@gmail.com>
2018-11-07 01:57:19 +0900

    Modify

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params['id'])
   end
 
   def new
@@ -12,15 +13,43 @@ class TasksController < ApplicationController
   end
 
   def create
-    raise
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      # redirect_to new_task_path
+      render :new
+      # render_to_string :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @task.update(task_params)
+
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if @task.destroy
+      redirect_to tasks_path
+    else
+      redirect_to task_path(@task)
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params['id'])
   end
 end
